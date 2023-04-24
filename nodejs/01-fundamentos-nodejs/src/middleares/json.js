c334193eba083d2aff0ead5e77e857024f66f421
Pedro Moreno Chagas Ligeiro Nascimento <nennogabriel@gmail.com>
2023-04-24 19:26:16 -0300

    nodejs : projeto 01 : modulo 3: aula 06

---
+++ nodejs/01-fundamentos-nodejs/src/middleares/json.js
@@ -0,0 +1,17 @@
+export async function json(request, response, next) {
+  const buffers = [];
+
+  for await (const chunk of request) {
+    buffers.push(chunk);
+  }
+
+  try {
+    request.body = JSON.parse(Buffer.concat(buffers).toString());
+  } catch (error) {
+    request.body = null;
+  }
+
+  response.setHeader("Content-Type", "application/json");
+
+  next();
+}
--- nodejs/01-fundamentos-nodejs/src/server.js
+++ nodejs/01-fundamentos-nodejs/src/server.js
@@ -1,21 +1,12 @@
 import http from "node:http";
+import { json } from "./middleares/json.js";
 
 const users = [];
 
 const server = http.createServer(async (request, response) => {
   const { url, method } = request;
 
-  const buffers = [];
-
-  for await (const chunk of request) {
-    buffers.push(chunk);
-  }
-
-  try {
-    request.body = JSON.parse(Buffer.concat(buffers).toString());
-  } catch (error) {
-    request.body = null;
-  }
+  await json(request, response, () => {});
 
   if (method === "GET" && url === "/users") {
     return response.setHeader("Content-Type", "application/json").end(JSON.stringify(users));
