6a75a1d6181c64b0b6cc546de2df5ffba6b52c60
Pedro Moreno Chagas Ligeiro Nascimento <nennogabriel@gmail.com>
2023-04-27 13:18:47 -0300

    nodejs : projeto 02 : modulo 02 : aula 02

--- nodejs/02-api-rest/src/server.ts
+++ nodejs/02-api-rest/src/server.ts
@@ -1,9 +1,12 @@
 import fastify from 'fastify'
+import { knex } from './database'
 
 const app = fastify()
 
-app.get('/', async (request, reply) => {
-  return { hello: 'world!' }
+app.get('/hello', async (request, reply) => {
+  const tables = await knex('sqlite_schema').select('*')
+
+  return { tables }
 })
 
 app
