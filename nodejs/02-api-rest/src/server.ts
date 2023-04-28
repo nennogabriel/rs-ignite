65b04c4fa97671cff99d078c2ae0e81c2a8ccbf7
Pedro Moreno Chagas Ligeiro Nascimento <nennogabriel@gmail.com>
2023-04-27 22:29:16 -0300

    nodejs : projeto 02 : modulo 02 : aula 05

--- nodejs/02-api-rest/src/server.ts
+++ nodejs/02-api-rest/src/server.ts
@@ -1,12 +1,19 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
 import { knex } from './database'
 
 const app = fastify()
 
 app.get('/hello', async (request, reply) => {
-  const tables = await knex('sqlite_schema').select('*')
+  const transactions = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Teste de transação',
+      amount: 100,
+    })
+    .returning('*')
 
-  return { tables }
+  return { transactions }
 })
 
 app
