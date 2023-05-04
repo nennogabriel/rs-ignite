e82b0e146c44f93ac61ebe559e2f2f5c27625aaf
Pedro Moreno <nennogabriel@gmail.com>
2023-05-04 10:57:45 -0300

    nodejs: projeto 03 : modulo 03 : aula 04

--- nodejs/03-api-solid/src/http/controllers/register.ts
+++ nodejs/03-api-solid/src/http/controllers/register.ts
@@ -1,6 +1,5 @@
-import { prisma } from "@/lib/prisma"
+import { registerUseCase } from "@/use-cases/register"
 import { FastifyReply, FastifyRequest } from "fastify"
-import { hash } from "bcryptjs"
 import { z } from "zod"
 
 export async function register (request: FastifyRequest, reply: FastifyReply) {
@@ -12,27 +11,12 @@ export async function register (request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body)
 
-  const password_hash = await hash(password, 2)
-
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  })
-
-  if (userWithSameEmail) {
-    return reply.status(409).send({
-      message: 'User already exists',
-    })
+  try{
+    await registerUseCase({ name, email, password })
   }
-
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash: password,
-    },
-  })
+  catch (error) {
+    return reply.status(409).send()
+  }
 
   return reply.status(201).send()
 }
