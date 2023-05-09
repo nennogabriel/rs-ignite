dfeb405f46ad50661a0f3a786dfd8de8640ae9d2
Pedro Moreno <nennogabriel@gmail.com>
2023-05-09 17:22:51 -0300

    nodejs: projeto 03 : modulo 06 : aula 06

--- nodejs/03-api-solid/src/http/controllers/profile.ts
+++ nodejs/03-api-solid/src/http/controllers/profile.ts
@@ -1,7 +1,12 @@
+import { makeGetUserProfileUseCase } from '@/use-cases/factoriers/make-get-user-profile-use-case';
 import { FastifyRequest, FastifyReply } from 'fastify';
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  request.jwtVerify()
-  console.log(request.user.sub)
-  return reply.status(200).send();
+
+  const getUserProfileUseCase = makeGetUserProfileUseCase()
+  const { user:userDB } = await getUserProfileUseCase.execute({ userId: request.user.sub })
+
+  const { password_hash, ...user } = userDB
+
+  return reply.status(200).send({ user })
 }
