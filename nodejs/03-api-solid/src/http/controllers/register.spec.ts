2ca437f7753ba54767d729e4370947eac3cfce1e
Pedro Moreno <nennogabriel@gmail.com>
2023-05-10 15:25:57 -0300

    nodejs: projeto 03 : modulo 06 : aula 10

--- nodejs/03-api-solid/src/http/controllers/register.spec.ts
+++ nodejs/03-api-solid/src/http/controllers/register.spec.ts
@@ -1,3 +1,26 @@
-import { test } from 'vitest'
+import { app } from '@/app'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('register', async () => {})
+
+describe('Register (e2e)', async () => {
+
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should register a new user', async () => {
+    const response = await request(app.server).post('/users')
+      .send({
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: '123456'
+      })
+
+    expect(response.status).toBe(201)
+  })
+})
