2006312c28eaf9057c026b819abe5252814ae6b3
Pedro Moreno <nennogabriel@gmail.com>
2023-05-18 08:51:08 -0300

    nlw : spacetime :  server : 03

--- nlw/spacetime/server/src/server.ts
+++ nlw/spacetime/server/src/server.ts
@@ -1,14 +1,29 @@
-import { PrismaClient } from '@prisma/client'
+import 'dotenv/config'
+
 import fastify from 'fastify'
+import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
-const prisma = new PrismaClient()
 
+app.register(cors, {
+  origin: true,
+})
+
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
   .listen({
     port: 3333,
+    host: '0.0.0.0',
   })
-  .then(() => console.log('🚀 Server is running on http://localhost:3333'))
+  .then(() => {
+    console.log('🚀 HTTP server running on port http://localhost:3333')
+  })
